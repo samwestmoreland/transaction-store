ff1d3358b0b1d1722b93d1bfb6f26cf35f731427
Sam Westmoreland <samwestmoreland@192.168.1.115>
2024-10-28 19:32:43 +0000

    Terraform for k3d and aws

---
+++ terraform/aws/aws.tf
@@ -0,0 +1,177 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Main VPC"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Main IGW"
+  }
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "Main NAT Gateway"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc   = true
+  count = 1
+  tags = {
+    Name = "NAT EIP"
+  }
+}
+
+resource "aws_security_group" "app" {
+  name   = "Application Security Group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "Main Cluster"
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "app-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn        = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "name": "app",
+    "image": "ghcr.io/samwestmoreland/tx-store-server:latest",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 8080,
+        "hostPort": 8080
+      }
+    ]
+  }
+]
+DEFINITION
+}
+
+# Create an ECS service
+resource "aws_ecs_service" "app" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "app"
+    container_port   = 80
+  }
+
+  network_configuration {
+    subnets          = [aws_subnet.private.id]
+    security_groups = [aws_security_group.app.id]
+  }
+}
+
+# Create a load balancer
+resource "aws_lb" "app" {
+  name               = "App Load Balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.app.id]
+  subnets            = [aws_subnet.public.id, aws_subnet.private.id]
+}
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "App Target Group"
+  port        = 80
+  protocol     = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+}
+
+# Create IAM role for ECS task execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
---
+++ terraform/k3d/main.tf
@@ -0,0 +1,31 @@
+provider "docker" {}
+
+resource "null_resource" "cluster" {
+  for_each = toset(var.k3d_cluster_name)
+  triggers = {
+    agent_count   = var.agent_count
+    server_count  = var.server_count
+    ip            = var.k3d_cluster_ip
+    port          = var.k3d_cluster_port
+    k3s_version   = var.k3s_version
+  }
+  provisioner "local-exec" {
+    command = "k3d cluster create ${each.key} --agents ${var.agent_count} --servers ${var.server_count} --api-port ${var.k3d_cluster_ip}:${var.k3d_cluster_port} --port ${var.k3d_host_lb_port}:${var.k3d_cluster_lb_port}@loadbalancer --image rancher/k3s:${var.k3s_version}"
+  }
+}
+
+resource "null_resource" "cluster_delete" {
+  for_each = toset(var.k3d_cluster_name)
+  provisioner "local-exec" {
+    command = "k3d cluster delete ${each.key}"
+    when    = destroy
+  }
+}
+
+data "docker_network" "k3d" {
+  for_each = toset(var.k3d_cluster_name)
+  depends_on = [
+    null_resource.cluster
+  ]
+  name = "k3d-${each.key}"
+}
